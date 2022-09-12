4c3621f0188d70b507b16844320551760f201ea1
Shaddai Adeniran <shaddaidan@gmail.com>
2022-09-12 16:28:11 -0700

    gg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,12 @@
-# include <stdio.h>
-
+#include<stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
-		char Mystr[] = "\"Programming is like building a multilingual puzzle";
-
-
-
-			puts(Mystr);
-
-				return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 
 }
