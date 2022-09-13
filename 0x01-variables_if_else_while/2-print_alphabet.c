3655508e61ee4defd677146adaccfdcddeb20c03
Shaddai Adeniran <shaddaidan@gmail.com>
2022-09-12 18:18:51 -0700

    sef

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stfio.h>
+/**
+* maim -Entry point
+*
+* Return: Always 0 (success/correct)
+*/
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; aplhabet++)
+{
+putcher(alphabet);
+}
+
+putcher('\n');
+
+return (0);
+
+}
